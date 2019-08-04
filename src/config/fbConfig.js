40998c3c4ac83b7581940d083aba4531ae6af00e
Erick Trettel <ericktrettel@gmail.com>
2019-08-04 16:07:03 -0300

    Adding Data to Firestore

--- src/config/fbConfig.js
+++ src/config/fbConfig.js
@@ -1,6 +1,6 @@
 import firebase from 'firebase/app';
 import 'firebase/firestore';
-import 'firebase/auth;
+import 'firebase/auth';
 
 // Your web app's Firebase configuration
 var firebaseConfig = {
