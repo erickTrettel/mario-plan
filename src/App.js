41e53a6c2f232fc060088717388d130afa2adecd
Erick Trettel <ericktrettel@gmail.com>
2019-08-04 12:44:16 -0300

    Dashboard Component

--- src/App.js
+++ src/App.js
@@ -1,12 +1,16 @@
 import React from 'react';
 import Navbar from './components/layout/Navbar';
-import { BrowserRouter } from 'react-router-dom';
+import Dashboard from './components/dashboard/Dashboard';
+import { BrowserRouter, Switch, Route } from 'react-router-dom';
 
 function App() {
   return (
     <BrowserRouter>
       <div className="App">
         <Navbar />
+        <Switch>
+          <Route to='/' component={Dashboard} />
+        </Switch>
       </div>
     </BrowserRouter>
   );
