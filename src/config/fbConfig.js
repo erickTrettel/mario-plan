4290ba48403b79ad66bb120452c0c3baf46bb680
Erick Trettel <ericktrettel@gmail.com>
2019-08-04 15:46:35 -0300

    Creating a Firebase project

---
+++ src/config/fbConfig.js
@@ -0,0 +1,19 @@
+import firebase from 'firebase/app';
+import 'firebase/firestore';
+import 'firebase/auth;
+
+// Your web app's Firebase configuration
+var firebaseConfig = {
+  apiKey: "AIzaSyBcda2cazh6mAu9DxMRTXYtQv0L1UtENjM",
+  authDomain: "net-ninja-marioplan-288f6.firebaseapp.com",
+  databaseURL: "https://net-ninja-marioplan-288f6.firebaseio.com",
+  projectId: "net-ninja-marioplan-288f6",
+  storageBucket: "",
+  messagingSenderId: "241254183173",
+  appId: "1:241254183173:web:a3e8d4636db1ca83"
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+firebase.firestore().settings({ timestampsInSnapshots: true });
+
+export default firebase;
